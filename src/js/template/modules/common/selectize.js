50aa96804a4b998c8e0ba20f260b05343818bc0e
PipZip <37911683+PipZip@users.noreply.github.com>
2020-01-22 22:31:34 +0600

    sync

--- src/js/template/modules/common/selectize.js
+++ src/js/template/modules/common/selectize.js
@@ -5,6 +5,9 @@
 export const selectize = async () => {
 	await import(/* webpackChunkName: "selectize" */ 'selectize');
 
-	$('.js-select').selectize();
+	$('.js-select:not([multiple])').selectize();
+	$('.js-select[multiple]').selectize({
+		plugins: ['remove_button']
+	});
 	$('.selectize-input input').prop('readonly', true);
 };
